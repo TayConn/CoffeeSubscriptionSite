72ac4241ad75713610accdcd9f5ef3a031ff7ad7
Taylor <tayjoconn@gmail.com>
2021-03-03 16:21:45 -0800

    duplicate

--- script.js
+++
@@ -1,7 +0,0 @@
-//Mobile Menu JS
-const hamburger = document.getElementById('hamburger');
-const navUL = document.getElementById('nav-ul');
-
-hamburger.addEventListener('click', () => {
-    navUL.classList.toggle('show');
-});
